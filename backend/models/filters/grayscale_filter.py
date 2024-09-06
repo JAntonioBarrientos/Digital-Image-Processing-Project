c0bf699238d47e5e52f9f46438d300b257504a70
Jose Antonio Barrientos Sanchez <a.barrientos@ciencias.unam.mx>
2024-09-05 23:19:30 -0600

    Gray filter funciona

--- backend/models/filters/grayscale_filter.py
+++ backend/models/filters/grayscale_filter.py
@@ -1,9 +1,24 @@
-from PIL import ImageOps
+from PIL import Image
 
 class GrayscaleFilter:
     def __init__(self, image):
         self.image = image
 
-    # Método que aplica el filtro de escala de grises
+    # Método que aplica el filtro de escala de grises de manera manual
     def apply_filter(self):
-        return ImageOps.grayscale(self.image)
+        # Convertir la imagen a formato RGB si no lo está
+        img = self.image.convert('RGB')
+        pixels = img.load()  # Cargar los píxeles de la imagen
+        
+        width, height = img.size  # Obtener las dimensiones de la imagen
+        
+        # Iterar sobre cada píxel
+        for i in range(width):
+            for j in range(height):
+                r, g, b = pixels[i, j]  # Obtener los valores RGB del píxel
+                p = (r + g + b) // 3  # Promediar los valores RGB para obtener escala de grises
+                pixels[i, j] = (p, p, p)  # Asignar el nuevo valor de píxel en escala de grises
+
+        return img  # Retornar la imagen procesada
+
+    
